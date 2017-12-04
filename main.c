8fbd07e0896b165ae3560f1eb0e67343d846f059
Alex Lu <Alex@Alexs-MBP-2.fios-router.home>
2017-12-04 02:16:00 -0500

    im going nuts

--- main.c
+++ main.c
@@ -7,18 +7,77 @@
 #include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <signal.h>
+
 #define wipe printf("\033[H\033[J")
 #define NTOKS 32
 //#define NCMDS 1
 
-
-int main(int argc, char **argv) {
+void run(char**, int, int);
+int main() {
 
   init();
-  loop();
-  
-  return 0;
+  char dir[64];
+
+  char * input;
+  while(17){
+    getcwd(dir, sizeof(dir));
+    printf("%s$ ", dir);
+    input = readline();
+    if(strcmp(input, "exit") == 0){
+      exit(1);
+    }
+    else if (strstr(input, "cd")){
+        char** args = tokenize(input, " ");
+        chdir(args[1]);
+    }
+    else{
+      //loop with strsep through all the semicolons
+      char * hermes;
+      hermes = strsep(&input, ";");
+      //check for redirect stdout
+      if(strchr(hermes, '>')){
+        char * bashpart;
+        bashpart = strsep(&hermes, ">");
+        char ** args = tokenize(bashpart, " ");
+        while(hermes[0] == ' '){
+          hermes++;
+        }
+        int fd = creat(hermes, 0777);
+        int a = dup(1);
+        dup2(fd, 1);
+        run(args, a, 0);
+      }
+      if(strchr(hermes, '<')){
+        char* bashpart;
+        bashpart = strsep(&hermes, "<");
+        char** args = tokenize(bashpart, " ");
+        while(hermes[0] == ' '){
+          hermes++;
+        }
+        int fd;
+        fd = open(hermes, O_RDONLY | O_CREAT, 0777);
+        int b = dup(0);
+        dup2(fd, 0);
+        run(args, 1, b);
+    }
+
+
+  else{
+    char** args = tokenize(hermes, " ");
+    run(args, 1, 0);
+  }
+
+
+
+
+    }
+  }
+return 0;  
 }
+
 
 void init() {
   //wipe;
@@ -41,67 +100,81 @@ void loop() {
     
     printf("%s$ ", dir);
     input = readline();
-    char * inputDIE1;
-    char * inputDIE2;
-    inputDIE1 = input;
-    inputDIE2 = input;
-    if(strchr(inputDIE1, '>') || strchr(inputDIE2, '<')){
-      //int pd;
-      int pd = fork();
-      printf("got forked");
-      if(pd == -1) {
-        printf("well shit");
-        return;
-      }
-      if(pd == 0) {
-        printf("check!");
-        cmds = tokenize(input, ";");
-        while(cmds[i]){
-          args = tokenize(cmds[i], " \n\t");
-          int k;
-          int h;
-          for(k = 0; k<6; k++){
-            if(strcmp(args[k], ">" ) == 0){
-              h = k;
-              k = 6;
-            }
-            k++;
+
+    if(strcmp(input, "exit") == 0){
+      exit(0);
+    }
+    if(strstr(input, "cd")){
+      args = tokenize(input, "cd");
+      chdir(args[1]);
+      }  
+    else{
+    cmds = tokenize(input,";");
+     while(cmds[i]) {
+
+
+      printf("test1\n");
+       if(strchr(cmds[i], ">") != NULL){
+         char* bashpart = strsep(&cmds[i], ">");
+         char** moddedargs = tokenize(cmds[i], " ");
+         printf("test\n");
+         while(cmds[i][0] == ' '){
+           cmds[i]++; //creds to michael lee for helping me fix bug w spaces at front 
+         }
+         int fd = open(cmds[i], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+         int a = dup(1);
+         dup2(fd, 1);
+         if(!fork()){
+           execvp(moddedargs[0], moddedargs);
+         }
+         else{
+           int status;
+           wait(&status);
+           if(a != 1){
+             dup2(a, 1);
+           }
+         }
+       }
+
+      if(strchr(cmds[i], "<")){
+        char* bashpart = strsep(&cmds[i], "<");
+        char** moddedargs = tokenize(cmds[i], " ");
+        while(cmds[i][0] == ' '){
+          cmds[i]++; 
+        }
+        if(!(strcmp(moddedargs[0], ""))){
+          return;
+        }
+        int fd = open(cmds[i], O_RDONLY | O_CREAT, 0777);
+        int b = dup(0);
+        dup2(fd, 0);
+        if(!fork()){
+          execvp(moddedargs[0], moddedargs);
+        }
+        else{
+          int status;
+          wait(&status);
+          if(b != 0){
+            dup2(b, 0);
           }
-          printf("made it 1");
-        if(strchr(cmds[i], '>') != NULL){
-          int fdout;
-          char* name = args[k+1];
-          fdout = open(name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-          dup2(fdout, 1);
-          close(fdout);
-          printf("made it 2");
-        }/*
-        if(strchr(cmds[i], '<') != NULL){
-          int fdint;
-          fdint = open(input1, O_RDONLY, 0);
-          dup2(fdint, 0);
-          close(fdint);
         }
-        */
-        }
+      }
+
+      else{
+
+      args = tokenize(cmds[i]," \n\t");
+      if(!fork()){
+        execvp(args[0], args);
       }
       else{
-        waitpid(pd, 0, 0);
+        int status;
+        wait(&status);
       }
-    }
-      
-
-
-
-
-    cmds = tokenize(input,";");
-    while(cmds[i]) {
-      args = tokenize(cmds[i]," \n\t");
+      }
       i++;
-      if(!func(args)) {
-	return;
-      }
-    }
+      
+     }
+     }
   }
 }
 
@@ -192,6 +265,21 @@ int exc(char** args) {
   return 1;
 }
 
+void run(char ** args, int a, int b){
+  if(!fork()){
+    execvp(args[0], args);
+  }
+  else{
+    int status;
+    wait(&status);
+    if(a != 1){
+      dup2(a, 1);
+    }
+    if(b != 0){
+      dup2(b, 0);
+    }
+  }
+}
 //func checker
 int func(char** args) {
   int i;
